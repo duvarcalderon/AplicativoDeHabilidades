3756534872f7a228a8e9874e480d3621dc9bc554
Duvar Calderon <dfcf739@gmail.com>
2022-10-24 10:53:37 -0500

    Commentary in home page for user validation

--- Funcionalidades/validar.php
+++ Funcionalidades/validar.php
@@ -5,7 +5,7 @@ $password=$_POST['password'];
 $conexion=mysqli_connect("localhost","root","","desarrollo");
 $consulta="SELECT * FROM registro WHERE correo='correo' and password='password'";
 $resultado=mysqli_query($conexion,$consulta);
-print_r($_POST);
+//print_r($_POST);
 if (($correo=='administrador@ucc.com.co')&&($password=='administradorucc')) {
          header('Refresh: 1; URL=tregistrados.php');
 
