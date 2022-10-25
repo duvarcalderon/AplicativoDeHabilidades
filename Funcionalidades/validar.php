b9effa06df6ca676becbd001a83f6a67fb4b474e
Duvar Calderon <dfcf739@gmail.com>
2022-10-24 21:36:26 -0500

    User Validation for start session in app

--- Funcionalidades/validar.php
+++ Funcionalidades/validar.php
@@ -2,28 +2,36 @@
 $correo=$_POST['correo'];
 $password=$_POST['password'];
 
-$conexion=mysqli_connect("localhost","root","","desarrollo");
-$consulta="SELECT * FROM registro WHERE correo='correo' and password='password'";
-$resultado=mysqli_query($conexion,$consulta);
-//print_r($_POST);
-if (($correo=='administrador@ucc.com.co')&&($password=='administradorucc')) {
-         header('Refresh: 1; URL=tregistrados.php');
+$conexion=new mysqli("localhost","root","","desarrollo");
+$consulta= " SELECT * FROM registro WHERE correo = '".$correo."' and password = '".$password."'";
+$resultado = $conexion->query($consulta);
+$rows=mysqli_num_rows($resultado);
+if ($rows > 0) {
+  if (($correo=='administrador@ucc.com.co')&&($password=='administradorucc')) {
+          header('Refresh: 1; URL=tregistrados.php');
 
-    echo  '<script>
-    alert("Bienvenido Admministrador UCC,espere un momento");
-    </script>';
-    exit;
+      echo  '<script>
+      alert("Bienvenido Admministrador UCC,espere un momento");
+      </script>';
+      exit;
+  }else{
+
+    if($correo=$_POST['correo'] && $password=$_POST['password']){
+
+            header('Refresh: 1; URL=../indexCuerpo.php');
+
+      echo  '<script>
+      alert("Bienvenido Estudiante UCC,espere un momento");
+      </script>';
+      exit;
+    }
+  }
 }else{
-
-  if($correo=$_POST['correo'] && $password=$_POST['password']){
-
-          header('Refresh: 1; URL=../indexCuerpo.php');
-
-    echo  '<script>
-    alert("Bienvenido Estudiante UCC,espere un momento");
-    </script>';
-    exit;
-}
+  header('Refresh: 1; URL=regusuario.php');
+  echo  '<script>
+  alert("Usuario no registrado ... Lo invitamos a REGISTARSE");
+  </script>';
+  exit;
 }
 
 ?>
